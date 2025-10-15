399325639325413ef8364662c6f0894c2a0f0b55
CaioMiranda12 <caioclm04@gmail.com>
2025-10-15 17:51:59 -0300

    feat: add DTO types based on API

---
+++ src/types/index.ts
@@ -0,0 +1,23 @@
+export interface ActivityDTO {
+  id: number;
+  description: string;
+  completed: boolean;
+  dueDate: string;
+}
+
+export interface GroupDTO {
+  id: number;
+  name: string;
+  activities: ActivityDTO[];
+}
+
+export interface CreateGroupDTO {
+  name: string;
+}
+
+export interface CreateActivityDTO {
+  description: string;
+  dueDate: string;
+  completed: boolean;
+  groupId: number;
+}
